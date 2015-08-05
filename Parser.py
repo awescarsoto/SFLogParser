dddbef85caf41cf796ce63e442646590da220700
oxsc <oxsc@pge.com>
2015-08-05 14:14:17 -0700

    + Text file with markers of entries and exits for tree levels + Text file showing which index of array contains important split info for markers + Parser splits lines and compares for method entries. Tabs denote level.

--- Parser.py
+++ Parser.py
@@ -1,1 +1,22 @@
 __author__ = 'oxsc'
+
+# Open the file for reading
+f = open("C:\Users\oxsc\Desktop\Log.txt", "r")
+
+currLevel = 0
+
+# Iterate over the lines in the file
+for line in f:
+    if "|" in line:
+        splitLine = line.split("|")
+        if splitLine[1] == "METHOD_ENTRY":
+            currLevel += 1
+            print currLevel * "\t" + splitLine[4]
+        if splitLine[1] == "CONSTRUCTOR_ENTRY":
+            currLevel += 1
+            print currLevel * "\t" + splitLine[3]
+        if splitLine[1] == "METHOD_EXIT":
+            currLevel -= 1
+        if splitLine[1] == "CONSTRUCTOR_EXIT":
+            currLevel -= 1
+
