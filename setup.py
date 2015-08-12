dc103cc4f23bea6599ac9be81ebe2f22751d48d4
oxsc <oxsc@pge.com>
2015-08-12 13:50:30 -0700

    + added C DLL files

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 __author__ = 'oxsc'
 
 from distutils.core import setup
+from glob import glob
 import py2exe
 
-setup(console=['Parser.py'])
+data_files = [("Microsoft.VC90.CRT", glob(r'C:\Users\oxsc\Desktop\Programs\Calibre Portable\Calibre\Microsoft.VC90.CRT\*.*'))]
+setup(data_files=data_files, console=['Parser.py'])
